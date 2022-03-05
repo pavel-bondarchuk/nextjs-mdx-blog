8a93996a42f3e1c064e827064793dd84d9094347
Pavel Bondarchuk <bonddesign85@gmail.com>
2022-03-05 22:47:13 +0200

    .

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = nextConfig
+module.exports = {
+  assetPrefix: isProd ? '/nextjs-mdx-blog/' : ''
+}
